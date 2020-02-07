5c3af98ae42cd9746609969480400fc80eb79653
skrrrm <a@macbook-windows>
2020-02-07 11:27:38 +0100

    Add project files.

---
+++ KEAOpgave2.2LommeregnerAndenSolution/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace KEAOpgave2._2LommeregnerAndenSolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLommeregner = new System.Windows.Forms.Label();
+            this.labelVejledning = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLommeregner
+            // 
+            this.labelLommeregner.AutoSize = true;
+            this.labelLommeregner.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLommeregner.Location = new System.Drawing.Point(215, 68);
+            this.labelLommeregner.Name = "labelLommeregner";
+            this.labelLommeregner.Size = new System.Drawing.Size(290, 51);
+            this.labelLommeregner.TabIndex = 0;
+            this.labelLommeregner.Text = "Lommeregner";
+            // 
+            // labelVejledning
+            // 
+            this.labelVejledning.AutoSize = true;
+            this.labelVejledning.Location = new System.Drawing.Point(215, 141);
+            this.labelVejledning.Name = "labelVejledning";
+            this.labelVejledning.Size = new System.Drawing.Size(259, 25);
+            this.labelVejledning.TabIndex = 1;
+            this.labelVejledning.Text = "Skriv ét tal i hver tekstbox";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(219, 450);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 25);
+            this.labelResult.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(220, 240);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 4;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(220, 300);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(52, 48);
+            this.buttonPlus.TabIndex = 5;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(278, 300);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(52, 48);
+            this.buttonMinus.TabIndex = 6;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(336, 300);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(52, 48);
+            this.buttonMultiply.TabIndex = 7;
+            this.buttonMultiply.Text = "*";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.Location = new System.Drawing.Point(394, 300);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(52, 48);
+            this.buttonDivide.TabIndex = 8;
+            this.buttonDivide.Text = "/";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(568, 300);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(90, 48);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.Location = new System.Drawing.Point(478, 300);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(52, 48);
+            this.buttonSum.TabIndex = 10;
+            this.buttonSum.Text = "=";
+            this.buttonSum.UseVisualStyleBackColor = true;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 674);
+            this.Controls.Add(this.buttonSum);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelVejledning);
+            this.Controls.Add(this.labelLommeregner);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLommeregner;
+        private System.Windows.Forms.Label labelVejledning;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSum;
+    }
+}
+
---
+++ KEAOpgave2.2LommeregnerAndenSolution/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave2._2LommeregnerAndenSolution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Variabler
+        double a, b, result;
+        bool plus = false, minus = false, multiply = false, divide = false;
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            labelResult.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            plus = false; minus = false; multiply = false; divide = false;
+            a = 0; b = 0; result = 0;
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            minus = true;
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            multiply = true;
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            divide = true;
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            plus = true;
+        }
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            if (plus == true || minus == true || multiply == true || divide == true)
+            {
+                try
+                {
+                    a = Convert.ToDouble(textBox1.Text); // Konvertere textbox1 til en double, så der kan regnes på det, da det jo er en string
+                    b = Convert.ToDouble(textBox2.Text); // --------- || ---------
+                }
+                catch
+                {
+                    labelResult.Text = "Indtast kun tal";
+                }
+            }
+            else
+            {
+                labelResult.Text = "Klik på et tegn for at udregne.";
+            }
+
+            if (plus == true)
+            {
+                result = a + b; // Lægger a og b sammen og konvertere til tekst
+                labelResult.Text = Convert.ToString(result); // Konvertere 'res' til en string så den kan smækkes ind i 'labelResult'
+            }
+
+            else if (minus == true)
+            {
+                result = a - b; // Lægger a og b sammen og konvertere til tekst
+                labelResult.Text = Convert.ToString(result); // Konvertere 'res' til en string så den kan smækkes ind i 'labelResult'
+            }
+
+            else if (multiply == true)
+            {
+                result = a * b; // Lægger a og b sammen og konvertere til tekst
+                labelResult.Text = Convert.ToString(result); // Konvertere 'res' til en string så den kan smækkes ind i 'labelResult'
+            }
+            else if (divide == true)
+            {
+                result = a / b; // Lægger a og b sammen og konvertere til tekst
+                labelResult.Text = Convert.ToString(result); // Konvertere 'res' til en string så den kan smækkes ind i 'labelResult'
+            }
+        }
+
+        
+    }
+}
